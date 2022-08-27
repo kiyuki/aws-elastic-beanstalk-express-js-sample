79e0c88b8258bba908b59af0b1c2a748bac33a2f
kiyuki <temp@test.com>
2022-08-27 11:05:07 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Test Ebi ebi !'));
+app.get('/', (req, res) => res.send('Full Pile Line message !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
